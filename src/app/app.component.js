4a5e1935f1bc746ebe089724ca7fdd731b3e3c34
GVS <jvasquez@gvscolombia.com>
2017-03-06 16:53:57 -0500

    Se separa el template de detalles de heroes para que sea reusable en toda la aplicacion, se importa desde el AppComponent

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,12 +9,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
-}());
-exports.Hero = Hero;
 var HEROES = [
     { id: 11, name: 'Mr. Nice' },
     { id: 12, name: 'Narco' },
@@ -38,7 +32,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  \t\t\t<h1>{{title}}</h1>\n  \t\t\t<h2>My Heroes</h2>\n  \t\t\t<ul class=\"heroes\">\n\t\t\t\t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t\t</li>\n  \t\t\t</ul>\n  \t\t\t<div *ngIf=\"selectedHero\">\n\t  \t\t\t<h2>{{selectedHero.name}} details!</h2>\n\t  \t\t\t<div>\n\t  \t\t\t\t<label>id: </label>{{selectedHero.id}}\n\t  \t\t\t</div>\n\t  \t\t\t<div>\n\t  \t\t\t\t<label>name: </label><input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n\t  \t\t\t</div>\n\t  \t\t</div>\n  \t\t\t",
+            template: "\n  \t\t\t<h1>{{title}}</h1>\n  \t\t\t<h2>My Heroes</h2>\n  \t\t\t<ul class=\"heroes\">\n\t\t\t\t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t\t</li>\n  \t\t\t</ul>\n  \t\t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  \t\t\t",
             styles: ["\n\t\t.selected{\n\t\t\tbackground-color: #CFD8DC !important;\n\t\t\tcolor: white;\n\t\t}\n\t\t.heroes{\n\t\t\tmargin: 0 0 2em 0;\n\t\t\tlist-style: none;\n\t\t\tpadding: 0;\n\t\t\twidth: 15em;\n\t\t}\n\t\t.heroes li{\n\t\t\tcursor: pointer;\n\t\t\tposition: relative;\n\t\t\tleft: 0;\n\t\t\tbackground-color: #EEE;\n\t\t\tmargin: .5em;\n\t\t\tpadding: .3em 0;\n\t\t\theight: 1.6em;\n\t\t\tborder-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover{\n\t\t\tbackground-color: #BBD8DC !important;\n\t\t\tcolor: white;\n\t\t}\n\t\t.heroes li:hover{\n\t\t\tcolor: #607D8B;\n\t\t\tbackground-color: #DDD;\n\t\t\tleft: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\t    position: relative;\n\t\t    top: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\t    display: inline-block;\n\t\t    font-size: small;\n\t\t    color: white;\n\t\t    padding: 0.8em 0.7em 0 0.7em;\n\t\t    background-color: #607D8B;\n\t\t    line-height: 1em;\n\t\t    position: relative;\n\t\t    left: -1px;\n\t\t    top: -4px;\n\t\t    height: 1.8em;\n\t\t    margin-right: .8em;\n\t\t    border-radius: 4px 0 0 4px;\n\t\t}\n\n  \t"],
         }), 
         __metadata('design:paramtypes', [])
